261ff851580509e5f1b43c920034303413e02c79
dimoskib <89027581+dimoskib@users.noreply.github.com>
2021-12-21 08:16:28 +0100

    Add files via upload

---
+++ new_module/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Courses theme",
+    'summary': """""",
+    'description': """A theme for teachers in courses""",
+    'author': "Bobo",
+    'website': "http://www.yourcompany.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Theme/Creative',
+    'version': '1.0',
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/assets.xml',
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/snippets.xml',
+        'views/options.xml',
+        'views/sidebar.xml',
+        # 'views/courses_chatter.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ new_module/static/src/js/tutorial_editor.js
@@ -0,0 +1,12 @@
+odoo.define('new_module.s_testimonial_options', function (require) {
+    var options = require('web_editor.snippets.options');
+});
+
+//odoo.define('new_module.s_testimonial_options', function (require) {
+//    var options = require('web_editor.snippets.options');
+//    options.registry.s_testimonial_options = options.Class.extend({
+//        onFocus: function () {
+//            alert("On focus!")
+//        },
+//    });
+//});
